b78ed92ac6567ada6c6c043afeea9deaf6a48a30
romanjbenitez <roman200217@gmail.com>
2023-10-03 23:51:57 -0300

    Remove duplicate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <main className='relative h-screen w-full'>
       <BackgroundAnimation />
-      <BackgroundAnimation />
       <Intro />
 
       <About />
